9fd19918a38b0c7aa06e7086585ba81a59a9684e
ajiganteng <ajibaguss88@gmail.com>
2026-04-02 11:59:26 +0700

    PDQ1

--- hitungIP.java
+++ hitungIP.java
@@ -5,20 +5,49 @@ public class hitungIP {
 
         //variabel
         Double IPS=0.0;
+        int SKS=0;
 
         //input IPS
         System.out.println("Masukkan IPS: ");
         IPS = scanner.nextDouble();
 
         //percabangan
-        if (IPS >= 3.50) {
-            System.out.println("SKS= 24");
+        if (IPS <= 4 && IPS >= 3.50) {
+            System.out.println("SKS yang dapat di ambil 24");
+            SKS=24;
         } else if (IPS <= 3.49 && IPS >= 3.00) {
-            System.out.println("SKS= 22");
+            System.out.println("SKS yang dapat diambil 22");
+            SKS=22;
         } else if (IPS <= 2.99 && IPS >= 2.00) {
-            System.out.println("SKS= 20");
-        } else if (IPS <2.00) {
-            System.out.println("SKS= 18");
+            System.out.println("SKS yang dapat diambil 20");
+            SKS=20;
+        } else if (IPS <2.00 && IPS >=0) {
+            System.out.println("SKS yang dpaat diambil= 18");
+            SKS=18;
+        } else {
+            System.out.println("IPS tidak valid!");
+            System.out.println("Tidak ada SKS yang dapat diambil");
+            SKS=0;
         }
+
+        //switch case
+        switch (SKS) {
+            case 24:
+                System.out.println("Baik Sekali");
+                break;
+            case 22:
+                System.out.println("Baik");
+                break;
+            case 20:
+                System.out.println("Cukup");
+                break;
+            case 18:
+                System.out.println("Kurang");
+                break;
+        
+            default:
+                break;
+        }
+        
     }
 }
