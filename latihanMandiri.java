3def0355c2fab20d48800cffd76c0b04baaad2df
ajibaguss88-hub <ajibaguss88@gmail.com>
2026-04-23 12:16:34 +0700

    Add files via upload

---
+++ latihanMandiri.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+public class latihanMandiri {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // // //soal 1
+        // System.out.print("Masukkan jumlah data: ");
+        // int n = sc.nextInt();
+
+        // int[] angka = new int[n];
+
+        // for (int i = 0; i < n; i++) {
+        //     System.out.print("Masukkan angka ke-" + i + ": ");
+        //     angka[i] = sc.nextInt();
+        // }
+
+        // int max = angka[0];
+
+        // for (int i = 1; i < angka.length; i++) {
+        //     if (angka[i] > max) {
+        //         max = angka[i];
+        //     }
+        // }
+
+        // System.out.println("Nilai tertinggi: " + max);
+
+        //soal 2
+        // System.out.print("Masukkan jumlah data: ");
+        // int n = sc.nextInt();
+
+        // int[] angka = new int[n];
+
+        // for (int i = 1; i < n; i++) {
+        //     System.out.print("Masukkan angka ke-" + i + ": ");
+        //     angka[i] = sc.nextInt();
+        // }
+
+        // System.out.print("Masukkan angka yang dicari: ");
+        // int cari = sc.nextInt();
+
+        // boolean ditemukan = false;
+
+        // for (int i = 0; i < angka.length; i++) {
+        //     if (angka[i] == cari) {
+        //         ditemukan = true;
+        //         break;
+        //     }
+        // }
+
+        // if (ditemukan) {
+        //     System.out.println("Angka ditemukan dalam array");
+        // } else {
+        //     System.out.println("Angka tidak ditemukan");
+        // }
+
+        //soal 3
+        System.out.print("Masukkan jumlah data: ");
+        int n = sc.nextInt();
+
+        int[] angka = new int[n];
+
+        for (int i = 1; i < n; i++) {
+            System.out.print("Masukkan angka ke-" + i + ": ");
+            angka[i] = sc.nextInt();
+        }
+
+        for (int i = angka.length - 1; i >= 0; i--) {
+            System.out.print(angka[i] + " ");
+        }
+    
+        
+    }
+}
